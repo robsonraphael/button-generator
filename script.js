343322d2778dfe5d5aadaab03e2a007b5c2deca1
Robson Raphael <79436128+robsonraphael@users.noreply.github.com>
2022-06-06 22:23:23 -0300

    fix script

--- script.js
+++ script.js
@@ -1,52 +1,66 @@
-const controls = document.getElementById("controls");
-console.log(controls);
-const btn = document.getElementById("btn");
-const description = document.getElementById("description");
+const controls = document.getElementById('controls');
+const button = document.getElementById('btn');
+const cssText = document.getElementById('description');
+const description = document.getElementById('description');
 
-const appendStyle = {
-  text: document.getElementById("text"),
-  color: document.getElementById("color"),
-  background: document.getElementById("backgroundColor"),
-  height: document.getElementById("height"),
-  width: document.getElementById("width"),
-  borderRadius: document.getElementById("borderRadius"),
-  fontFamily: document.getElementById("fontFamily"),
-  fontSize: document.getElementById("fontSize"),
-};
+controls.addEventListener("change", fastChange)
+button.addEventListener("click", () => { alert('Toc Toc ...') })
 
-for(element in appendStyle){
-  let doc = document.createElement('span');
-  doc.innerText = `${element}: ${appendStyle[element].value}`
-  description.appendChild(doc);
+const handleStyle = {
+  element: btn,
+  backgroundColor(value) {
+    this.element.style.backgroundColor = value;
+  },
+  height(value) {
+    this.element.style.height = value + 'px';
+  },
+  width(value) {
+    this.element.style.width = value + 'px';
+  },
+  texto(value) {
+    this.element.innerText = value;
+  },
+  color(value) {
+    this.element.style.color = value;
+  },
+  border(value) {
+    this.element.style.border = value;
+  },
+  borderRadius(value) {
+    this.element.style.borderRadius = value + 'px';
+  },
+  fontFamily(value) {
+    this.element.style.fontFamily = value;
+  },
+  fontSize(value) {
+    this.element.style.fontSize = value + 'px';
+  },
 }
 
-controls.addEventListener("change", () => {
-  for (element in appendStyle) {
-    if (
-      element === "height" ||
-      element === "width" ||
-      element === "fontSize" ||
-      element === "borderRadius"
-    ) {
-      if (element === "fontSize") {
-        btn.style.setProperty("font-size", appendStyle[element].value + "px");
-      }
-      if (element === "borderRadius") {
-        btn.style.setProperty(
-          "border-radius",
-          appendStyle[element].value + "px"
-        );
-      }
-      btn.style.setProperty(element, appendStyle[element].value + "px");
-    } else {
-      if (element === "text") {
-        btn.innerText = appendStyle[element].value;
-      }
-      if (element === "fontFamily") {
-        btn.style.setProperty("font-family", appendStyle[element].value);
-      }
-      btn.style.setProperty(element, appendStyle[element].value);
-    }
-  }
-});
+function fastChange(e) {
+  const name = e.target.name
+  const value = e.target.value
 
+  handleStyle[name](value)
+  saveValues(name, value)
+  showCss();
+}
+
+// Local storage
+const saveValues = (name, value) => {
+  localStorage[name] = value;
+}
+
+// setar valor
+const setValues = () => {
+  const properties = Object.keys(localStorage);
+  properties.forEach(props => {
+    handleStyle[props](localStorage[props])
+    controls.elements[props].value = localStorage[props]
+  });
+  showCss();
+}
+
+const showCss = () => {
+  cssText.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';</span> <span>')
+}
